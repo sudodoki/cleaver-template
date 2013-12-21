bbc2547c9bcec43ecd6096084b2c709b5416aecc
sudodoki <smd.deluzion@gmail.com>
2013-12-21 17:10:22 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+  global.port = 8080
+  grunt.initConfig({
+    watch: {
+      all: {
+        options: {
+          livereload: 35353
+        },
+        files: ["<%= grunt.option(\"filename\") || 'index' %>.md"],
+        tasks: "shell:presentation:<%= grunt.option(\"filename\") %>"
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          open: "http://localhost:<%= global.port %>/<%= grunt.option(\"filename\") || 'index' %>.html",
+          port: global.port,
+          livereload: 35353
+        }
+      }
+    },
+    shell: {
+      presentation: {
+        command: function (filename) {
+          if (!filename) {filename = 'index'};
+          console.log('building your presenation ' + filename + '.md')
+          return "node_modules/.bin/cleaver " + filename + '.md'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('build', 'watch and build presentation by filename', function(filename) {
+    if (!filename) {filename = 'index'};
+    grunt.option('filename', filename)
+    grunt.task.run('default')
+  })
+  grunt.registerTask('default', ['shell:presentation', 'connect:server', 'watch:all']);
+}
--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 cleaver-template
 ================
 
-Jump start your cleaver presentation in no time.
+Jump start your [cleaver](https://github.com/jdan/cleaver) presentation in no time.
+
+Reinventing `cleaver watch` functionality but also sprinkling it with [livereload](http://livereload.com/) & [static server](https://npmjs.org/package/connect) functionality via awesome grunt packages (see package.json dependency list for names).
+
+
+How to run this?
+================
+
+1. `npm install` to install all dependencies.
+2a. `grunt` to output compiled index.md to index.html
+2b. `grunt build:<Name of you md file>` to output compiled <name>.md to output and have that.
+Both tasks will also start the static server at 8080 and open it in your browser
+
+Have any question/proposals?
+============================
+
+Don't hesitate to open issue, or just to do regular fork-feature branch-pull request thing on me.
